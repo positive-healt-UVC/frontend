c380b7c8ec2d5aae1bac2edbeeb5989baa55dc71
ItsDimitrie <laue0002@HZ.nl>
2023-11-15 14:04:55 +0100

    Homepage version 1.0.0 and added components.

---
+++ src/lib/components/Buttons.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let href;
+  export let text;
+</script>
+
+<div>
+  <div class="grid grid-cols-1 gap-4 mb-5 mx-5">
+    <a href={href};
+          class="w-30 flex items-center text-center 
+          justify-center px-8 py-3 
+          border border-transparent 
+          text-base font-medium 
+          rounded-xl 
+          text-white 
+          button-color 
+          md:py-4 md:text-lg md:px-10">
+          {text}
+    </a>
+  </div>
+</div>
+
+<style>
+  .bg-sky {
+    background-image: url(".//imgs/BG_Light_Blue.png");
+    background-position: center;
+  }
+
+  .button-color {
+    background-color: #FF9D00 ;
+  }
+
+  .button-color:hover {
+    background-color: #00B0D1 ;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,29 @@
 	import "../app.css";
 </script>
 
+
+
 <div class="flex flex-col h-screen justify-between">
 	<main>
 		<slot />
+		
 	</main>
 
+	<footer class="text-center">
+		<a href="/"><i class="fa-solid fa-house fa-2xl p-5 mb-2"></i></a>
+		<i class="fa-brands fa-accessible-icon fa-2xl p-5 mb-2"></i>
+		<i class="fa-solid fa-user fa-2xl p-5 mb-2"></i>
+		<i class="fa-solid fa-gear fa-2xl p-5 mb-2"></i>
+	</footer>
+	
 </div>
 
 <style>
+	footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+	}
 </style>
