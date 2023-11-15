73897ba1cbdc6a8d3bae9b997f17e89590b0a930
Erik van den  Broek <broe0137@hz.nl>
2023-11-15 14:48:02 +0100

    added event component, and simple first page for agenda

---
+++ src/lib/components/Agenda.svelte
@@ -0,0 +1,3 @@
+<script></script>
+
+<div class="bg-slate-500 rounded-md h-16">01</div>
---
+++ src/routes/agenda/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+
+    import Agenda from "$lib/components/Agenda.svelte";
+
+</script>
+<div class="min-h-screen bg-sky flex flex-col items-center">
+    <div class="bg-slate-500">
+        <p>menu</p>
+    </div>
+    <div class="bg-yellow-500 w-5/6">
+    <div class="grid grid-cols-1 gap-4 w-5/6 mt-10 mx-auto mb-10">
+        <Agenda />
+        <Agenda />
+        <Agenda />
+      </div>
+    </div>
+    
+</div>
+  
+  
+    <style>
+      .bg-sky {
+        background-image: url(".//imgs/BG_Light_Blue.png");
+        background-position: center;
+      }
+  
+      .button-color {
+        background-color: #0C9EFF ;
+      }
+    </style>
+  
