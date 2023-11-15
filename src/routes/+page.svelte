56442f982b3510ef4dbf69bc6f1b05187df27589
ItsDimitrie <laue0002@HZ.nl>
2023-11-15 10:27:41 +0100

    Added Frontend Boilerplate

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Example from '$lib/components/Example.svelte';
+
+</script>
+
+<div class="container w-full mx-auto">
+    <h1>Welcome to Sveltekit</h1>
+    <Example />
+</div>
+
+<style>
+</style>
