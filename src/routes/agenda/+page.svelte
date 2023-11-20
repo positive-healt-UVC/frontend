a58496ebc79c105e6eeee95cdc84792d4e5d6142
Erik van den  Broek <broe0137@hz.nl>
2023-11-20 14:02:15 +0100

    fixed code formatting

--- src/routes/agenda/+page.svelte
+++ src/routes/agenda/+page.svelte
@@ -1,33 +1,49 @@
 <script>
-
-    import Agenda from "$lib/components/Agenda.svelte";
-    import WeekNumber from "$lib/components/WeekNumber.svelte";
-
+  import Agenda from "$lib/components/Agenda.svelte";
+  import WeekNumber from "$lib/components/WeekNumber.svelte";
 </script>
+
 <div class="bg-sky">
   <div class="min-h-screen flex flex-col items-center">
-  <div class="w-5/6">
-    <WeekNumber />
-  </div>
+    <div class="w-5/6">
+      <WeekNumber />
+    </div>
     <div class="w-5/6 rounded-md">
-    <div class="grid grid-cols-1 gap-4 w-5/6 mt-10 mx-auto mb-20">
-        <Agenda naamActiviteit="Zwemmen" startTijd="10:30" eindTijd="11:30" locatie="De Aanloop 1, 4335 AT Middelburg"/>
-        <Agenda naamActiviteit="Voetbal" startTijd="12:30" eindTijd="13:30" locatie="De Aanloop 1, 4335 AT Middelburg"/>
-        <Agenda naamActiviteit="Zwemmen" startTijd="10:30" eindTijd="11:30" locatie="De Aanloop 1, 4335 AT Middelburg"/>
-        <Agenda naamActiviteit="Voetbal" startTijd="12:30" eindTijd="13:30" locatie="De Aanloop 1, 4335 AT Middelburg"/>
+      <div class="grid grid-cols-1 gap-4 w-5/6 mt-10 mx-auto mb-20">
+        <Agenda
+          naamActiviteit="Zwemmen"
+          startTijd="10:30"
+          eindTijd="11:30"
+          locatie="De Aanloop 1, 4335 AT Middelburg"
+        />
+        <Agenda
+          naamActiviteit="Voetbal"
+          startTijd="12:30"
+          eindTijd="13:30"
+          locatie="De Aanloop 1, 4335 AT Middelburg"
+        />
+        <Agenda
+          naamActiviteit="Zwemmen"
+          startTijd="10:30"
+          eindTijd="11:30"
+          locatie="De Aanloop 1, 4335 AT Middelburg"
+        />
+        <Agenda
+          naamActiviteit="Voetbal"
+          startTijd="12:30"
+          eindTijd="13:30"
+          locatie="De Aanloop 1, 4335 AT Middelburg"
+        />
         <Agenda />
         <Agenda />
       </div>
-    </div> 
-</div> 
+    </div>
+  </div>
 </div>
 
-  
-  
-    <style>
-      .bg-sky {
-        background-image: url(".//imgs/BG_Light_Blue.png");
-        background-position: center;
-      }
-    </style>
-  
+<style>
+  .bg-sky {
+    background-image: url(".//imgs/BG_Light_Blue.png");
+    background-position: center;
+  }
+</style>
