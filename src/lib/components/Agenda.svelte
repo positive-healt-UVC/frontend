e969589f2e644fbb95a7007d87697e8b8e701476
lardo-tech <hond0017@hz.nl>
2023-11-20 11:28:39 +0100

    agenda blokken gemaakt

--- src/lib/components/Agenda.svelte
+++ src/lib/components/Agenda.svelte
@@ -1,6 +1,19 @@
-<script></script>
+<script>
+    export let naamActiviteit = "Naam activiteit";
+    export let startTijd = "Begin tijd";
+    export let eindTijd = "Eind tijd";
+    export let locatie = "Locatie";
+</script>
 
-<div class="button-color rounded-md h-16">01</div>
+<div class="button-color rounded-md h-32">
+    <h1 class="text-center text-lg mt-2 mb-1 text-white">{naamActiviteit}</h1>
+    <div class="text-sm text-white ml-2">
+        <p>Begin tijd: {startTijd}</p>
+        <p>Eind tijd:{eindTijd}</p>
+        <p class="mt-2">{locatie}</p>
+    </div>
+    <p class="text-xs text-sky-600 float-right mb-1 mr-5"><a href="activiteit"><button>More ></button></a></p>
+</div>
 
 <style>
     .button-color {
--- src/routes/agenda/+page.svelte
+++ src/routes/agenda/+page.svelte
@@ -14,12 +14,8 @@
   </div>
     <div class="bg-white w-5/6 rounded-md">
     <div class="grid grid-cols-1 gap-4 w-5/6 mt-10 mx-auto mb-10">
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
+        <Agenda naamActiviteit="Zwemmen" startTijd="10:30" eindTijd="11:30" locatie="De Aanloop 1, 4335 AT Middelburg"/>
+        <Agenda naamActiviteit="Voetbal" startTijd="12:30" eindTijd="13:30" locatie="De Aanloop 1, 4335 AT Middelburg"/>
         <Agenda />
       </div>
     </div> 
