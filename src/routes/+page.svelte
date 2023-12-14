7c82acd183a88a9dd4ed0d9b9107704d55b436e4
ItsDimitrie <laue0002@HZ.nl>
2023-12-14 10:47:30 +0100

    Added fade animation to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,39 @@
 <script>
   import { onMount } from 'svelte';
   import Buttons from '$lib/components/Buttons.svelte';
-  import { fly} from 'svelte/transition';
 
   let currentIndex = 0;
   let text = [
     { text: 'Ability Link transforms communication for individuals with disabilities, providing a user-friendly platform tailored to their needs.' },
     { text: 'Ability Link revolutionizes communication for people with disabilities, offering an accessible and intuitive platform that empowers seamless interaction.' },
     { text: 'Ability Link empowers individuals with disabilities by revolutionizing communication through its accessible and intuitive platform.' }
-
   ];
 
   let feedbackUser = [
     { feedbackUser: 'Berend Botje' },
     { feedbackUser: 'Arend Zijlstra' },
     { feedbackUser: 'Piet Paulisma' },
-
   ];
 
   let profileImage = [
     { profileImage: './imgs/Profile1.jpg' },
     { profileImage: './imgs/Profile2.png' },
     { profileImage: './imgs/Profile3.png' },
-
   ];
+
+  let fadeIn = false;
 
   onMount(() => {
     const interval = setInterval(() => {
       currentIndex = (currentIndex + 1) % text.length;
+      fadeIn = !fadeIn; // Toggle the value to trigger the fade-in animation
     }, 5000);
 
     return () => clearInterval(interval);
   });
 </script>
 
-<body transition:fly={{ y: 0, duration: 300 }}>
+<body>
   <div class="min-h-screen bg-sky flex flex-col items-center">
 
     <img
@@ -55,7 +54,7 @@
       </p>
     </div>
 
-    <div class=" text-center text-xs mt-6 -mb-4 ml-4 mr-4">
+    <div class="text-center text-xs mt-6 -mb-4 ml-4 mr-4">
       <p>
         Gelieve aan te melden of te registreren om verder te gaan.
       </p>
@@ -66,16 +65,17 @@
       <Buttons text={"Registeren"} href={"/register"}/>
     </div>
 
-    <div>
+    <div class="{fadeIn ? 'fade-in' : ''}">
       <img class="rounded-3xl mt-10 w-15 h-12" src="{profileImage[currentIndex].profileImage}" alt="ProfileImage"/>
     </div>
-
-    <div class="text-center mt-3">
+    
+    <div class="text-center mt-3 {fadeIn ? 'fade-in' : ''}">
       <div>
         <p class="ml-2 mr-2 text-xs">"{text[currentIndex].text}"</p>
         <p class="mt-2 text-xs italic">- {feedbackUser[currentIndex].feedbackUser}</p>
       </div>
     </div>
+
   </div>
 </body>
 
@@ -83,5 +83,18 @@
   .bg-sky {
     background-image: url("./imgs/BG_Light_Blue.png");
     background-position: center;
+  }
+
+  .fade-in {
+    animation: fadeIn 2s ease-in-out;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
   }
 </style>
